ee7539d9fec4aaa40a05446428e00a76c00414c2
Cao Hoang Phuc <caophuc1612@gmail.com>
2026-06-04 22:14:32 +0700

    fix: GroupRepository, ProjectRepository

--- Backend/src/main/java/com/example/se330/repository/ProjectRepository.java
+++ Backend/src/main/java/com/example/se330/repository/ProjectRepository.java
@@ -12,11 +12,3 @@ import com.example.se330.entity.Project;
 public interface ProjectRepository extends JpaRepository<Project, Long> {
     List<Project> findByCourse(Course course);
 }
-import com.example.se330.entity.Project;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ProjectRepository
-        extends JpaRepository<Project, Long> {
-}
